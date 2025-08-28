bdcb1b13979676a56dc6fdaaa468d6a51f13ec91
asmrt <56843306+asmrt@users.noreply.github.com>
2025-08-28 20:28:45 +0900

    Update week_65_solution.sql

--- week_65/week_65_solution.sql
+++ week_65/week_65_solution.sql
@@ -10,19 +10,20 @@
 -- 　今回の流れ：
 --   　1.マーケットプレイスから Cybersyn US Patent Grants のデータセットを取得する。
 --   　2．CybersynのSQLクエリを使って、Nvidiaに関連する特許を検索する（結果が多すぎないように上限10件にしています）。
---   　3．以下のルールに基づいて、特許に「合格（TRUE）/不合格（FALSE）」を返す関数を作る：
+--   　3．以下のルールに基づいて、特許に「TRUE(有効）/FALSE(有効ではない)」を返す関数を作る：
 --  　    <ルール>
---          申請日(APPLICATION_DATE) と発行日(PUBLICATION_DATE)の差が、以下に該当する場合はTRUE、それ以外はFALSEを返却する。
---  　　        Reissue（再発行）特許 → APPLICATION_DATE(申請) と PUBLICATION_DATE(発行日) の差が 365日以内
---  　　        Design（意匠）特許    → APPLICATION_DATE と PUBLICATION_DATE の差が2年(730日)以内
+--          出願日(APPLICATION_DATE) と公開日(PUBLICATION_DATE)の差が、以下に該当する場合はTRUE、それ以外はFALSEを返却する。
+--  　　        Reissue（再発行）特許 → APPLICATION_DATE(出願日) と PUBLICATION_DATE(公開日) の差が 365日以内
+--  　　        Design（意匠）特許    → APPLICATION_DATE(出願日) と PUBLICATION_DATE(公開日) の差が2年(730日)以内
 
--- → 色々な言語でUDFを作成でき、チャレンジ上の指定はないが、今回は「スカラSQL UDF」を作成、使ってみる流れとした。
+-- → 色々な言語でUDFを作成でき、チャレンジ上の指定はないが、今回は「スカラSQL UDF(入力行ごとに1)」を作成、使ってみる流れとした。
 
 
 -- 0.事前準備：各種設定、DB、スキーマ作成
 USE ROLE SYSADMIN;
-USE warehouse COMPUTE_WH;
+USE warehouse COMPUTE_WH_M; 
 
+-- ForstyFriday用のDATABASE、SCHEMAを作成
 CREATE OR REPLACE DATABASE FROSTY_FRIDAY_DB;
 CREATE OR REPLACE SCHEMA   WEEK_65_SCHEMA;
 
@@ -42,7 +43,6 @@ SELECT patent_index.patent_id
     , invention_title
     , patent_type
     , application_date 
-    , document_publication_date
 FROM cybersyn_us_patent_grants.cybersyn.uspto_contributor_index AS contributor_index
 INNER JOIN
     cybersyn_us_patent_grants.cybersyn.uspto_patent_contributor_relationships AS relationships
@@ -52,45 +52,18 @@ INNER JOIN
     ON relationships.patent_id = patent_index.patent_id
 WHERE contributor_index.contributor_name ILIKE 'NVIDIA CORPORATION'
     AND relationships.contribution_type = 'Assignee - United States Company Or Corporation'
+ORDER BY APPLICATION_DATE DESC
 LIMIT 10
 ;
-
--- Steven P. Jobs を寄与者(contributor)とする特許を検索する。
-SELECT
-    patent_index.patent_id,
-    invention_title,
-    patent_type,
-    application_date,
-    contributor_name
-FROM
-    cybersyn_us_patent_grants.cybersyn.uspto_contributor_index AS contributor_index
-INNER JOIN
-    cybersyn_us_patent_grants.cybersyn.uspto_patent_contributor_relationships AS relationships
-ON (contributor_index.contributor_id = relationships.contributor_id)
-INNER JOIN
-    cybersyn_us_patent_grants.cybersyn.uspto_patent_index AS patent_index
-ON relationships.patent_id = patent_index.patent_id
-WHERE
-    contributor_index.contributor_name ILIKE 'Steven P. Jobs'
-;
--- タイトルに OLED を含む特許をすべて取得する。
-SELECT
-    patent_id,
-    invention_title,
-    patent_type,
-    application_date
-FROM
-    cybersyn_us_patent_grants.cybersyn.uspto_patent_index
-WHERE
-    invention_title ILIKE ANY ('%OLED%');
-
 -- 2-1. スカラSQL UDF作成
--- 以下ルールに基づいて、特許ごとに「TRUE(合格)/FALE(不合格)」を返却するSQL UDF（ユーザ定義関数）を作成する
+-- 以下ルールに基づいて、特許ごとに「TRUE(有効)/FALE(有効ではない)」を返却するSQL UDF（ユーザ定義関数）を作成する
 --  　<ルール>
---     申請日(APPLICATION_DATE) と発行日(PUBLICATION_DATE)の差が、以下に該当する場合はTRUE、それ以外はFALSEを返却する。
---  　　Reissue（再発行）特許 → APPLICATION_DATE(申請) と PUBLICATION_DATE(発行日) の差が 365日以内
---  　　Design（意匠）特許    → APPLICATION_DATE と PUBLICATION_DATE の差が2年(730日)以内
+--     出願日(APPLICATION_DATE) と公開日(PUBLICATION_DATE)の差が、以下に該当する場合はTRUE、それ以外はFALSEを返却する。
+--  　　　Reissue（再発行）特許 → APPLICATION_DATE(出願日) と PUBLICATION_DATE(公開日) の差が 365日以内
+--  　　  Design（意匠）特許    → APPLICATION_DATE(出願日) と PUBLICATION_DATE(公開日) の差が2年(730日)以内
 
+--      ※スカラSQL UDF(UDFs):1回の呼び出しにつき、1行(レコード)を返却する
+--       （【比較】表形式 SQL UDF(UDTFs)：1回の呼び出しにつき、テーブルを返却する。）
 --      ※今回は、うるう年(366日になるケース)は考慮しないこととした。
 
 -- 参照マニュアル
@@ -103,7 +76,7 @@ WHERE
 --         https://docs.snowflake.com/ja/sql-reference/functions/lower
 --   TRIM：先頭、末尾の文字削除（デフォルトは単一空白文字
 --        https://docs.snowflake.com/ja/sql-reference/functions/trim
-CREATE OR REPLACE FUNCTION FROSTY_FRIDAY_DB.WEEK_65_SCHEMA.validate_patent_gap_no_memoizable(
+CREATE OR REPLACE FUNCTION FROSTY_FRIDAY_DB.WEEK_65_SCHEMA.VALIDATE_PATENT_GAP_NO_MEMOIZABLE(
     application_date DATE,
     publication_date DATE,
     patent_type STRING
@@ -120,6 +93,9 @@ CASE
     ELSE FALSE
 END
 $$;
+
+-- 作成したUDFが反映されていることの確認
+-- → データベースエクスプローラーで、対象UDFを検索して確認する。
 
 -- 3-1.UDFを使ってみる/特許タイプ：Reissue
 --  ※ NVIDIA CORPORATIONの特許では該当なしのようだったため、特許企業（団体?）を絞らず集計
@@ -140,13 +116,14 @@ INNER JOIN
     cybersyn_us_patent_grants.cybersyn.uspto_patent_index AS patent_index
     ON relationships.patent_id = patent_index.patent_id
 WHERE
-    relationships.contribution_type = 'Assignee - United States Company Or Corporation'
+    contributor_index.contributor_name ILIKE 'NVIDIA CORPORATION' -- NVIDIA CORPORATIONで絞ると対象0県なのでコメントアウトする。
+    AND relationships.contribution_type = 'Assignee - United States Company Or Corporation'
     AND patent_type = 'Reissue'
     AND FROSTY_FRIDAY_DB.WEEK_65_SCHEMA.validate_patent_gap_no_memoizable(application_date, document_publication_date, patent_type) -- TRUEの場合をチェックする用の条件
---    AND FROSTY_FRIDAY_DB.WEEK_65_SCHEMA.validate_patent_gap_no_memoizable(application_date, document_publication_date, patent_type) = FALSE -- FALSEの場合をチェックする用の条件
-
+    --AND FROSTY_FRIDAY_DB.WEEK_65_SCHEMA.validate_patent_gap_no_memoizable(application_date, document_publication_date, patent_type) = FALSE -- FALSEの場合をチェックする用の条件
     LIMIT 10
 ;
+
 
 -- 3-1.UDFを使ってみる/特許タイプ：Design
 SELECT patent_index.patent_id
@@ -170,6 +147,37 @@ WHERE
     AND patent_type = 'Design'
     AND FROSTY_FRIDAY_DB.WEEK_65_SCHEMA.validate_patent_gap_no_memoizable(application_date, document_publication_date, patent_type) -- TRUEの場合をチェックする用の条件
     --AND FROSTY_FRIDAY_DB.WEEK_65_SCHEMA.validate_patent_gap_no_memoizable(application_date, document_publication_date, patent_type) = FALSE -- FALSEの場合をチェックする用の条件
-LIMIT 10
+    ORDER BY application_date desc
+LIMIT 3
 ;
 
+------------------
+/*
+SELECT
+    QUERY_ID,
+    QUERY_TYPE,
+    CONVERT_TIMEZONE('Asia/Tokyo',START_TIME) AS START_TIME,
+    CONVERT_TIMEZONE('Asia/Tokyo',END_TIME) AS END_TIME,
+    (TOTAL_ELAPSED_TIME/ 1000.0) AS TOTAL_ELAPSED_TIME, -- 経過時間（ミリ秒 → 秒に変換)
+FROM
+    TABLE(INFORMATION_SCHEMA.QUERY_HISTORY_BY_SESSION(RESULT_LIMIT => 10))
+WHERE
+    QUERY_TYPE = 'SELECT' -- セッション設定(SESSION SET)のクエリは除外
+    AND QUERY_TEXT NOT LIKE '%INFORMATION_SCHEMA%' -- 実行時間計測のクエリ自体を除外
+ORDER BY START_TIME DESC
+;
+*/
+/*
+SELECT
+    query_id,
+    start_time,
+    total_elapsed_time,
+    child_queries_wait_time,
+    query_text
+FROM snowflake.account_usage.query_history
+WHERE start_time >= DATEADD('hour', -1, CURRENT_TIMESTAMP())
+    AND QUERY_TYPE = 'SELECT' -- セッション設定(SESSION SET)のクエリは除外
+    AND QUERY_TEXT NOT LIKE '%INFORMATION_SCHEMA%' -- 実行時間計測のクエリ自体を除外
+ORDER BY start_time DESC
+;
+*/
